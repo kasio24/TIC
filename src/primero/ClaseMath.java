d6f5478f377ae51dac56a1be57dade77d11837e5
JaimePelaez <Jaipelaez1392@gmail.com>
2021-06-29 09:29:13 -0500

    commit

--- src/primero/ClaseMath.java
+++ src/primero/ClaseMath.java
@@ -1,5 +1,18 @@
 package primero;
 
 public class ClaseMath {
-    
+    public static void main(String[] args) {
+        double x= Math.E;
+        double z= Math.PI;
+
+        //int a= Math.sqrt(9);
+        //int b= Math.pow(3, 10);
+        //int c= Math.abs(10.5);  // recibe como parametro float, int, double, long y así mismo devuelve el parametro.
+        double d= Math.sin(10.1);
+        double e= Math.cos(5.0);
+        double f= Math.tan(3.5);
+        float g= Math.round(8.9999);
+        double h= Math.floor(8.666);
+        double i= Math.ceil(2.5);
+    }
 }
