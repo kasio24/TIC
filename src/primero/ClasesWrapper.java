4965efcd7e0981d9e41ef074148485264ca18606
JaimePelaez <Jaipelaez1392@gmail.com>
2021-06-29 10:35:07 -0500

    commit

--- src/primero/ClasesWrapper.java
+++ src/primero/ClasesWrapper.java
@@ -1,5 +1,28 @@
 package primero;
 
 public class ClasesWrapper {
+    public static void main(String[] args) {
+
+        Integer x = 34;
+        Integer y = Integer.getInteger("42");
+
+        Double w = 2.5;
+        
+
+        String numero ="2022";
+        String strTrue ="true";
+        String strFalse ="False";
+
+        int numeroInt = Integer.parseInt(numero);
+        float numeroFloat = Float.parseFloat(numero);
+        double numeroDouble = Double.parseDouble(numero);
+        long numeroLong = Long.parseLong(numero);
+        short numeroShort = Short.parseShort(numero);
+        boolean v = Boolean.parseBoolean(strTrue);
+        boolean f = Boolean.parseBoolean(strFalse);
     
+        String a= x.toString();
+        String b= w.toString();
+    
+    }
 }
